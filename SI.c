23378296cf06a953ceeb4b52f85f8ff20de95ebd
CHAITANYA <Chaitanyas0101@gmail.com>
2020-02-04 11:40:40 +0530

    Create SI.c

---
+++ SI.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int P,R,i;
+float t,SI;
+int = 0
+while(i < 3)
+{
+printf("enter the value of P,R,t");
+scanf("%d,%d,%f",&P,&R,&t);
+SI = (P*R*t)/100;
+printf("simple interest is");
+printf("%f",&SI);
+}
+}
